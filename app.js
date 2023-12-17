434530e2eaa406d2e31a38d611f4ee11a00ee91e
dinuka <dinukaattanayaka222@gmail.com>
2023-12-17 12:18:57 +0530

    complete weather app

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const express = require('express');
+
 const weather = require('./utils/weather');
-const geo =require('./utils/geo')
+const geo =require('./utils/geo');
+
+const app = express();
 // weather(79.8541134, 6.9387469)
 // .then((data)=>{
 //     console.log(data);
@@ -8,15 +12,46 @@ const geo =require('./utils/geo')
 //     console.log(error);
 // })
 
-geo('matara')
+// geo('matara')
+// .then((data)=>{
+//     //console.log(data);
+//     return weather(data.lat, data.lon)
+// })
+//     .then((data)=>{
+//         console.log(data)
+//     })
+// .catch((error)=>{
+//     console.log(error);
+//
+// })
+
+app.get('/' , (req,res)=>{
+    res.send("welcome home")
+});
+
+app.get('/about' , (req,res)=> {
+    res.send("about page")
+});
+app.get('/weather' , (req,res)=> {
+    if (!req.query.address) {
+        res.send("address not found")
+    }
+    geo(req.query.address)
 .then((data)=>{
     //console.log(data);
     return weather(data.lat, data.lon)
 })
     .then((data)=>{
-        console.log(data)
+       res.send(data);
     })
 .catch((error)=>{
-    console.log(error);
+    res.send(error);
 
 })
+
+
+});
+app.get('*' , (req,res)=> {
+    res.send("page not found")
+})
+app.listen(3000, ()=>console.log('server is on port 3000'))
